4878a498f2ff665b68840c8c24ab402d5c2a2026
Kergoc <concasseur-bureaux.2a@icloud.com>
2024-12-21 18:23:27 +0100

    ajout de la classe visualizer

--- utilities.py
+++ utilities.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import socket
+import time
 
-
+"""
 def fft_animation(signal, dt, ax, fig, buffer_size):
     line, = ax.plot([], [], lw=2) #empty lists because no initial x and y data for the line, lw defines line width
     ax.set_xlabel('Frequency (Hz)')
@@ -28,6 +29,7 @@ def fft_animation(signal, dt, ax, fig, buffer_size):
         plot_fft(buffer, delta_t_average, ax, line)
         return line,
     return FuncAnimation(fig, update_plot, interval=1000/24, blit=True)
+"""
 
 class HighPassFilter:
     def __init__(self, cutoff_frequency, dt):
@@ -59,8 +61,54 @@ class LowPassFilter:
         self.previous_sample = sample
         return filtered_sample
 
+class Visualizer:
+    def __init__(self, buffer, direction):
+        self.buffer = buffer
+        self.fig, self.ax = plt.subplots()
+        self.line = None  # Initialiser la ligne
+        self.direction = direction
 
-
+    def plot_FFT(self, xrangemin = 0, xrangemax = 20,  yrangemin = 0,  yrangemax = 25 ):
+        self.line, = self.ax.plot([], [], lw=2)
+        self.ax.set_xlabel('Frequency (Hz)')
+        self.ax.set_ylabel('Amplitude')
+        self.ax.set_title('Frequency Spectrum')
+        self.ax.set_xlim(xrangemin, xrangemax)
+        self.ax.set_ylim(yrangemin, yrangemax)
+        
+        return FuncAnimation(self.fig, self.update_plot_fft, interval=1000/24, blit=True) #si je return FuncAnimation il faudra créer dans le
+                                                                                      #programme principale un objet anim = visualizer.plot_FFT(dt=0.1)
+                                                                                      #puis exécuter plt.show
+    # Fonction interne pour mettre à jour l'animation
+    def update_plot_fft(self, frame):
+        delta_t_average = np.mean(self.buffer[0,0:])
+        # Calcul de la FFT
+        fft_values = np.fft.fft(self.buffer[self.direction,0:])
+        amplitude_spectrum = np.abs(fft_values) / len(fft_values)
+        frequencies = np.fft.fftfreq(len(fft_values), delta_t_average)
+        # Mise à jour des données de la ligne
+        self.line.set_data(frequencies[:len(frequencies)//2], amplitude_spectrum[:len(amplitude_spectrum)//2])
+        return self.line,
+    
+    def plot_time(self, duration = 15, refresh_rate = 24 ):
+        time_list = self.buffer[0,0:]
+        point_list = self.buffer[self.direction,0:]
+        self.line = self.ax.plot(time_list, point_list)[0]
+        self.ax.set_xlim([0, duration])  # Limites initiales de l'axe x
+        self.ax.set_ylim([-20, 20])  # Limites initiales de l'axe y
+        # Animation
+        anim = FuncAnimation(self.fig, self.update_plot_time, frames = duration * refresh_rate, interval=1000 / refresh_rate) #autre méthode,
+        plt.show()                                                                                      # la fonction ne renvoie rien mais lance 
+                                                                                                        #automatiquement l'animation
+                                                                                                        #quelle méthode est la mieux?
+    def update_plot_time(self, frame, xrange = 15):
+        if self.buffer[0,-1] > xrange: #je veux a voir une fenêtre glissante
+             self.ax.set_xlim(self.buffer[0,-1-xrange], self.buffer[0,-1])  # Décalage de l'axe
+        else:
+            self.ax.set_xlim(0, xrange)
+        
+        self.line.set_data(self.buffer[0,0:],self.buffer[self.direction,0,:])
+        return self.line,
 
 #get local ip (this function was made by chatgpt)
 def get_local_ip():
